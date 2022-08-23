3b2ad7e8ae4269e534529ff44913548251ec65a2
miqueiasdecco <miqueias@unicnet.com.br>
2022-08-23 14:43:25 -0300

    modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,10 @@ const server = express();
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+//Rotas
+
+
+server.listen(process.env.PORT);
