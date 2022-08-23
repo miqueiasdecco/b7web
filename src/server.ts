d6d8678ea3af6d66071edc457635a1af7b910ecd
miqueiasdecco <miqueias@unicnet.com.br>
2022-08-23 14:40:47 -0300

    renamed:    css/style.css -> public/css/style.css

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,9 @@ import mustache from 'mustache-express';
 import path from 'path';
 
 dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
